837d021fe0f45dfc849872c0d8a463f973ea61e7
Shamil <77851940+Shamil-B@users.noreply.github.com>
2023-03-06 22:40:17 +0300

    Add files via upload

---
+++ lyft-backend-engineering-main/batteries/spindler_battery.py
@@ -0,0 +1,12 @@
+from batteries.battery import Battery
+from utils import add_years
+
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date, current_date):
+        self.__last_service_date = last_service_date
+        self.__current_date = current_date
+
+    def needs_service(self) -> bool:
+        date_for_service = add_years(self.__last_service_date, 3)
+        return self.__current_date > date_for_service
